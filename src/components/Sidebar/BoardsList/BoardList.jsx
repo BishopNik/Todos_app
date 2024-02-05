c6556f69e77ff0836901c9bcc8f253a43680669e
Irina2304 <110673165+Irina2304@users.noreply.github.com>
2024-02-05 21:22:16 +0100

    listBoard scroll

--- src/components/Sidebar/BoardsList/BoardList.jsx
+++ src/components/Sidebar/BoardsList/BoardList.jsx
@@ -1,20 +1,27 @@
 /** @format */
 
-import React from 'react';
+import React, { useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { BoardItem } from '../BoardItem/BoardItem';
 import { ListContainer } from './BoardList.styled';
 
 export const BoardsList = () => {
-	const { board } = useParams(null);
-	return (
-		<>
-			<ListContainer>
-				{/* -----Here map---- */}
-				<BoardItem idBoard={board} />
-				<BoardItem idBoard={'00000'} />
-				<BoardItem idBoard={'33255'} />
-			</ListContainer>
-		</>
-	);
+  const { board } = useParams();
+  const listRef = useRef(null);
+
+  const handleScroll = event => {
+    const delta = event.deltaY;
+    listRef.current.scrollTop += delta;
+  };
+
+  return (
+    <>
+      <ListContainer onWheel={handleScroll} ref={listRef}>
+        {/* -----Here map---- */}
+        <BoardItem idBoard={board} />
+        <BoardItem idBoard={'00000'} />
+        <BoardItem idBoard={'33255'} />
+      </ListContainer>
+    </>
+  );
 };
